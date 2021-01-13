f5b3f657e8ca91cad91334e7b849d07e0c0f7aa3
Michael Epstein <mepstein6@users.noreply.github.com>
2021-01-12 22:28:41 -0500

    Create sample_for_loop.py

---
+++ OPSC-540-81_Security-Scripting-with-Python/Week 1/sample_for_loop.py
@@ -0,0 +1,28 @@
+
+if __name__ == '__main__':
+    spam = ['apples', 'bananas', 'tofu', 'cats']
+
+    spam_str = ''
+  
+    for spam_item in spam:
+      print("Spam item: " + spam_item)
+      spam_str = spam_str + spam_item
+      print("Spam str: " + spam_str)
+
+    print(spam_str)
+
+    cities = ['New York', 'Chicago', 'Atlanta', 'Burlington', 'Philadelphia', 'Denver']
+
+    cities_str = ''
+
+    # manipulate cities list here
+
+    print(cities_str)
+
+    food = ['carrots', 'mango', 'avocado']
+
+    food_str = ''
+ 
+    # manipulate food list here
+
+    print(food_str)
