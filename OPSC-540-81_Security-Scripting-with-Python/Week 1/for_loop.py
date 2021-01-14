24a231b9ee0b3f88ddab9249a322322f0d2252d5
Michael Epstein <mepstein6@users.noreply.github.com>
2021-01-14 18:53:54 -0500

    Completed Part 1 of assignment
    Completed for_loop and while_loop.

--- OPSC-540-81_Security-Scripting-with-Python/Week 1/for_loop.py
+++ OPSC-540-81_Security-Scripting-with-Python/Week 1/for_loop.py
@@ -1,2 +1,5 @@
 for i in range(0, 10):
-    print(i)
+    if i % 2 == 0:
+        print(i)
+    else:
+        continue
--- OPSC-540-81_Security-Scripting-with-Python/Week 1/while_loop.py
+++ OPSC-540-81_Security-Scripting-with-Python/Week 1/while_loop.py
@@ -1,4 +1,8 @@
 i = 0
 while(i < 10):
-    print(i)
-    i = i + 1
+    if i % 2 == 0:
+        print(i)
+        i = i + 1
+    else:
+        i = i + 1
+        continue
