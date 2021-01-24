241a24005c83284bb60d10e6d45c454fe2e4fd9b
Michael Epstein <mepstein6@users.noreply.github.com>
2021-01-23 21:16:22 -0500

    create da things

---
+++ OPSC-540-81_Security-Scripting-with-Python/Week 2/week2_prog-client_program.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Michael Epstein
+OPSC-540-81
+Client Program
+'''
+
+# import required modules
+from socket import *
+import sys
+from datetime import datetime as dt
+import ipaddress
+import argparse
+
+# code
