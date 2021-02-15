6ee6058deeacc6277892ef01d8499e89f7d5c393
Michael Epstein <mepstein19@gmail.com>
2021-02-14 21:47:02 -0500

    Update final-project-skeleton.py

--- OPSC-540-81_Security-Scripting-with-Python/Week 5/final-project-skeleton.py
+++ OPSC-540-81_Security-Scripting-with-Python/Week 5/final-project-skeleton.py
@@ -10,9 +10,11 @@ import ipaddress
 import nessrest
 import nmap
 import sys
+import gmail
+import time
 
 def get_inputs():
-    # create argparse arguments
+    # create argparse arguments, -i for cmd-line args; -f for input file.
     # check for cmd-line arguments, if present skip input file
     # check for input file
     # use cmd-line args or input file to identify IPs/CIDR
@@ -24,21 +26,36 @@ def nessus_scan_start(input)
     # Use default network scan as template, mark it to send email on completion. 
     # use input in place for hosts, start scan.
     # When scan is finished, email to user that scan is complete.
-    # return 'Scan Started: <SCAN_ID>' if started scan. Otherwise pass error message.
+    # return 'Scan Started: ', <SCAN_ID>. if started scan. Otherwise pass error message.
 
 def wait_email_user():
     # wait for email from nessus stating the scan is complete. 
-    # when this email is received, email the completed nessus scan document.
+    # use gmail library to check for email every 15 seconds.
+    # when this email is received, return True to main()
     # return email sent verification or error message
 
-def read_results():
-    # read report, if any highs or criticals are found, state the number of each within the email to the user.
+def read_results(scan_id):
+    # search nessus using scan_id
+    # read scan report, if any highs or criticals are found, state the number of each within the email to the user.
+    # return number of highs/mediums to main()
+
+def email_user():
+    # email scan report to user via nessus
+    # Nessus has already been preconfigured with an access key to gmail.
+    # print email_sent confirmation.
 
 def main():
     # retrieve inputs from get_inputs()
+    inputs = get_inputs()
     # pass inputs to nessus_start_scan()
-    # while loop on wait_email_user() to send the completed report to user when the Nessus scan is completed.
-    # print email sent verification or error message from wait_email_user() 
+    scan_id = nessus_scan_start(inputs)
+    # while loop on wait_email_user(), when True is received break while loop.
+    while wait_email_user() == False:
+        time.sleep(15)            
+    # read_results(scan_id) and pass the scan_id to search nessus for. Save result to variable as dictionary
+    read_results(scan_id)
+    # run email_user(scan_id) and pass in the scan_id. 
+    email_user(scan_id)
 
 if __name__ == "__main__":
     main()
